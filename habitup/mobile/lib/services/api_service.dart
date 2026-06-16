c35fa5bb0eabd1d8955111d1dbdd82e0141a5a5e
ercanaziz <ercanaziz08@gmail.com>
2026-06-16 20:32:16 +0300

    chore: BlueStacks yorumu temizlendi

--- habitup/mobile/lib/services/api_service.dart
+++ habitup/mobile/lib/services/api_service.dart
@@ -3,7 +3,7 @@ import 'package:http/http.dart' as http;
 import 'package:shared_preferences/shared_preferences.dart';
 
 class ApiService {
-  // BlueStacks için host makinenin Wi-Fi IP'si kullanılır
+  // Production URL
   static const String _base = 'https://habitup-staging-production.up.railway.app/api';
 
   static Future<String?> _token() async {
